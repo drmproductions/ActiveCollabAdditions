eee8ffb60c3ec34a897e73406749ace6cca34059
luveti <luveti@users.noreply.github.com>
2022-08-09 18:43:24 -0400

    Added utils.hash

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+export function hash(value) {
+	if (typeof value !== 'object') {
+		return value
+	}
+
+	const chunks = []
+	function append(value) {
+		if (Array.isArray(value)) {
+			for (const value2 of value) {
+				if (typeof value2 === 'object') {
+					append(value2)
+					continue
+				}
+				chunks.push(value2)
+			}
+			return
+		}
+
+		const keys = Object.keys(value)
+		keys.sort((a, b) => a.localeCompare(b))
+		for (const key of keys) {
+			chunks.push(key)
+			const value2 = value[key]
+			if (typeof value2 === 'object') {
+				append(value2)
+				continue
+			}
+			if (typeof value2 === 'string') {
+				chunks.push('"')
+			}
+			chunks.push(value2)
+		}
+	}
+	append(value)
+	return chunks.join('')
+}
