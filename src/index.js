a53c265d6ed94b6ed7c7774b6a3375b5d6121fe2
luveti <luveti@users.noreply.github.com>
2022-08-23 14:35:40 -0400

    Improved angie property waiting code
    - wait for all angie properties we depend on
    - check full property path each time (angie.collections gets overwrote
      at some point during load?)

--- src/index.js
+++ src/index.js
@@ -53,19 +53,43 @@ utils.call(async () => {
 	await waitForBody()
 	onUnload(() => style.init())
 
-	// sometimes these aren't loaded when we're injected
+	// we need to wait for several angie values to be populated before continuing
 	await onUnload(async () => {
+		function check(keys) {
+			let obj = angie
+			for (let i = 0; i < keys.length; i++) {
+				const key = keys[i]
+				if (obj[key] === undefined) {
+					return false
+				}
+				if (i < keys.length - 1) {
+					obj = obj[key]
+				}
+			}
+			return true
+		}
+
 		async function wait(key) {
-			while (!angie[key]) {
+			const keys = key.split('.')
+			while (!check(keys)) {
 				await utils.sleep(100)
 			}
 		}
+
 		await wait('api_url')
 		await wait('collections')
+		await wait('collections.job_types')
+		await wait('collections.users')
 		await wait('icons')
+		await wait('icons.main_menu_icon_system_settings')
+		await wait('icons.main_menu_icon_trash')
+		await wait('icons.svg_icons_cancel')
+		await wait('icons.svg_icons_icon_submit_time')
+		await wait('icons.svg_icons_recurring')
+		await wait('icons.svg_icons_star')
+		await wait('icons.svg_icons_time_tracker_icon')
 		await wait('user_session_data')
-		// TODO we may only need to wait for this?
-		await wait('all_loaded')
+		await wait('user_session_data.logged_user_id')
 	})
 
 	await onUnload(() => db.init())
